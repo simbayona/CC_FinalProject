7db7fb591482edf2bdacbad3a9c8bba4d2286e71
simbayona <60442765+simbayona@users.noreply.github.com>
2020-04-27 01:47:39 -0500

    making sizes a method

--- outer-draw.js
+++ outer-draw.js
@@ -1,15 +1,10 @@
 class Outer {
 
- constructor(type, size){
+ constructor(type){
  this.type = type; // this type variable lets me chose which one of the outer shapes I want to have
- this.size = size; // this size variable will let the user pick how big they want the outer limit to be
- this.outerWidth = 400 * this.size; // these four are just some math to make sure the circles and squares are the correct sizes and in the correct spot
- this.outerHeight = 400 * this.size;
- this.x = (width/2) - (this.outerWidth/2);
- this.y = (height/2) - (this.outerHeight/2);
  }
 
- display(){
+ display(){ // display just chooses which one of the drawer methods to pick from depending on the type
   if(this.type == "round"){
     this.roundOuter();
   }
@@ -20,20 +15,31 @@ class Outer {
     this.squareOuter();
   }
  }
-
+ resize(new_size){ // this method will let changing the size of the outer border much easier
+   this.size = new_size;
+ }
+ placement(){
+   this.outerWidth = 400 * this.size; // these four are just some math to make sure the circles and squares are the correct sizes and in the correct spot
+   this.outerHeight = 400 * this.size; // the first two determine the size based on a predetermined max
+   this.x = (width/2) - (this.outerWidth/2); // these two help for centering anything based on the rect function
+   this.y = (height/2) - (this.outerHeight/2);
+ }
  roundOuter(){
+   this.placement();
    fill("#B1FFA4");
    rect(this.x,this.y,this.outerWidth,this.outerHeight,50);
    fill(127);
    rect(this.x+10,this.y+10,this.outerWidth-20,this.outerHeight-20,50);
  }
  circleOuter(){
+   this.placement();
    fill("#B1FFA4");
    ellipse(width/2,height/2,this.outerWidth,this.outerHeight);
    fill(127);
    ellipse(width/2,height/2,this.outerWidth-15,this.outerHeight-15);
  }
  squareOuter(){
+   this.placement();
    fill("#B1FFA4");
    rect(this.x,this.y,this.outerWidth,this.outerHeight);
    fill(127);
--- sketch.js
+++ sketch.js
@@ -1,5 +1,3 @@
-let rounded;
-let circular;
 
 
 
@@ -7,61 +5,19 @@ let circular;
 function setup(){
   createCanvas(800,600);
   background(127);
-  rounded = new Outer("round",1);
-  circular = new Outer("circle",1);
-  square = new Outer("square",1);
+  roundedOuter = new Outer("round");
+  circularOuter = new Outer("circle");
+  squareOuter = new Outer("square");
 }
 
 function draw(){
   noStroke();
-  rounded.display();
-  //circular.display();
-  //square.display();
+  roundedOuter.resize(.5);
+  roundedOuter.display();
+  //circularOuter.display();
+  //squareOuter.display();
 }
 
 function mousePressed(){
   console.log(mouseX,mouseY); // position checker
 }
-
-// class Outer {
-//
-//  constructor(type, size){
-//  this.type = type; // this type variable lets me chose which one of the outer shapes I want to have
-//  this.size = size; // this size variable will let the user pick how big they want the outer limit to be
-//  this.outerWidth = 400 * this.size; // these four are just some math to make sure the circles and squares are the correct sizes and in the correct spot
-//  this.outerHeight = 400 * this.size;
-//  this.x = (width/2) - (this.outerWidth/2);
-//  this.y = (height/2) - (this.outerHeight/2);
-//  }
-//
-//  display(){
-//   if(this.type == "round"){
-//     this.roundOuter();
-//   }
-//   if(this.type == "circle"){
-//     this.circleOuter();
-//   }
-//   if(this.type == "square"){
-//     this.squareOuter();
-//   }
-//  }
-//
-//  roundOuter(){
-//    fill("#B1FFA4");
-//    rect(this.x,this.y,this.outerWidth,this.outerHeight,50);
-//    fill(127);
-//    rect(this.x+10,this.y+10,this.outerWidth-20,this.outerHeight-20,50);
-//  }
-//  circleOuter(){
-//    fill("#B1FFA4");
-//    ellipse(width/2,height/2,this.outerWidth,this.outerHeight);
-//    fill(127);
-//    ellipse(width/2,height/2,this.outerWidth-15,this.outerHeight-15);
-//  }
-//  squareOuter(){
-//    fill("#B1FFA4");
-//    rect(this.x,this.y,this.outerWidth,this.outerHeight);
-//    fill(127);
-//    rect(this.x+10,this.y+10,this.outerWidth-20,this.outerHeight-20);
-//  }
-// }
